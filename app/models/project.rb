5f5a2eefd930a8eb3e830d94fd1bd66e9fad551e
villainsff7 <villainsff790@gmail.com>
2022-03-03 05:24:39 +0000

    [Add] bootstrap,fontawesome

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,6 @@ class Project < ApplicationRecord
   belongs_to :members
   has_many :comments, dependent: :destroy
   has_many :thanks, dependent: :destroy
+  has_one_attached :project_image
   #タグ実装したら追加記載
 end
