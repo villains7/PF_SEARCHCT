3cce6c6f3cdb0fe7ba270a643cf46fe1784dcec4
villainsff7 <villainsff790@gmail.com>
2022-03-08 06:10:39 +0000

    [Update] search_controller

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,9 +1,16 @@
 class  Public::SearchesController < ApplicationController
   def search
-    @content = params[:content]
-    @records = Project.search_for(@content)
-    project_before = Project.where('created_at: <= ?', Time.now)
-    project_before.where(params[:create_at].all_month)
+    @keyword = params[:keyword] #入力されたキーワード
+    @region = params[:region]
+    @date = params[:date]
+    @records = Project.search_for(@keyword)
+    if @region.exist? && @date.exist?
+      @records = Project.where(region: @region,created_at: @date.all_month)
+    elsif @region.exist? && @date == ""
+      @records = Project.where(region: @region)
+    else
+      @records
+    end
     #Project.where(create_at:選択した月 1..31)
 
     # where 範囲を指定する方法
