e0ada1488cb624c0320df365a99bfdf7c1abd082
villainsff7 <villainsff790@gmail.com>
2022-03-10 14:09:47 +0000

    [Fix] my_page

--- app/models/member.rb
+++ app/models/member.rb
@@ -6,12 +6,11 @@ class Member < ApplicationRecord
   has_many :projects, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :thanks, dependent: :destroy
-  has_many :thanks_projects,through: :thanks,source: :project
   has_many :view_counts, dependent: :destroy
   has_one_attached :profile_image
 
   REGION = [["本社","本社"],["近畿","近畿"],["中部","中部"],["東北","東北"],["中国四国","中国四国"],["九州","九州"],["北海道","北海道"]]
-  SECION = [["営業部","営業部"],["業務部","業務部"],["経理部","経理部"]]
+  SECTION = [["営業部","営業部"],["業務部","業務部"],["経理部","経理部"]]
 
   def get_profile_image(weight,height)
     (profile_image.attached) ? profile_image : 'noimage.jpg'
