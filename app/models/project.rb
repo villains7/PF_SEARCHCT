95188a0ceeac039b0d42f3a8641d3e25d7c61689
villainsff7 <villainsff790@gmail.com>
2022-03-14 12:55:04 +0000

    [Fix] search_controller

--- app/models/project.rb
+++ app/models/project.rb
@@ -18,17 +18,4 @@ class Project < ApplicationRecord
     thanks.exists?(member_id: member.id)
   end
 
-  #プロジェクトの検索のメソッド
-  def self.search_for(keyword,content)
-    if keyword.present? && content.present?
-      Project.where('title LIKE ?', '%' + keyword + '%').where('salesman LIKE ?','%' + content + '%')
-    elsif keyword.present? && content == ""
-      Project.where('title LIKE ?', '%' + keyword + '%')
-    elsif keyword == "" && content.present?
-      Project.where('salesman LIKE ?','%' + content + '%')
-    else
-      Project.all
-    end
-  end
-
 end
