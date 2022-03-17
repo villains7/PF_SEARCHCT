366f8296ac24c569075f48448b2e24bc98287940
villainsff7 <villainsff790@gmail.com>
2022-03-17 06:08:45 +0000

    [Update] search_form

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,4 +1,5 @@
 class Public::SearchesController < ApplicationController
+  before_action :authenticate_member!
   def index
     @projects = Project.all
     # if (params[:tag_id]).present?
@@ -47,6 +48,58 @@ class Public::SearchesController < ApplicationController
       # date_selectで選択された値をparamsからTimeオブジェクトにする
       @date = Time.gm(year, month, day)
       @result = @result.where(created_at: [@date.at_beginning_of_month..@date.end_of_month])
+    end
+
+    # 登録番号検索
+    @registration_number = params[:registration_number]
+    if @registration_number.present?
+      @result = @result.where("registration_number like ? ", "%#{@registration_number}%")
+    end
+
+    # 車体番号検索
+    @registration_number = params[:body_number]
+    if @body_number.present?
+      @result = @result.where("body_number like ? ", "%#{@body_number}%")
+    end
+
+    # 車種で検索
+    @vehicle_type = params[:vehicle_type]
+    if @vehicle_type.present?
+      @result = @result.where(vehicle_type: @vehicle_type)
+    end
+
+    # リース種別で検索
+    @lease_type = params[:lease_type]
+    if @lease_type.present?
+      @result = @result.where(lease_type: @lease_type)
+    end
+
+    # 整備工場で検索
+    @body_shop = params[:body_shop]
+    if @body_shop.present?
+      @result = @result.where("body_shop like ? ", "%#{@body_shop}%")
+    end
+
+    # 保険有無
+    @insurance = params[:insurance]
+    if @insurance.present?
+      @result = @result.where(insurance: @insurance)
+    end
+
+    # リース終了日で検索
+    @end_year = params[:end_year]
+    @end_month = params[:end_month]
+    @end_day = params[:end_day]
+    if @end_year.present?
+      @result = @result.where(end_year: @end_year)
+    end
+
+    if @end_month.present?
+      @result = @result.where(end_month: @end_month)
+    end
+
+    if @end_day.present?
+      @result = @result.where(end_day: @end_day)
     end
 
     # チェックされたタグで検索
