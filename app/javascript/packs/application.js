5c34fb41b0fb25558d247001a597c30450b41ee0
villainsff7 <villainsff790@gmail.com>
2022-03-19 11:44:50 +0000

    [Fix] Jsfile

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,35 +12,7 @@ import "popper.js";
 import "bootstrap";
 import "../stylesheets/application"
 import '@fortawesome/fontawesome-free/js/all'
-import { Calendar } from '@fullcalendar/core';
-import dayGridPlugin from '@fullcalendar/daygrid';
 
-
-document.addEventListener('DOMContentLoaded', function() {
-  let calendarEl = document.getElementById("calendar");
-  let calendar = new Calendar(calendarEl, {
-    plugins: [dayGridPlugin],
-
-    locale: 'ja',
-    height: 'auto',
-    fixedWeekCount: false,
-    buttonText: {
-      today: '今日'
-    },
-    events :[
-      {
-        start:'2022-03-18',
-        end:'2022-03-18',
-        title: '口座振替締日',
-        display: 'background'
-      }
-    ],
-    dayCellContent: function(e) {
-    e.dayNumberText = e.dayNumberText.replace('日', '');
-    },
-  });
-  calendar.render();
-});
 
 Rails.start()
 Turbolinks.start()
---
+++ app/javascript/packs/calendar.js
@@ -0,0 +1,29 @@
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+
+
+document.addEventListener('turbolinks:load', function() {
+  let calendarEl = document.getElementById("calendar");
+  let calendar = new Calendar(calendarEl, {
+    plugins: [dayGridPlugin],
+
+    locale: 'ja',
+    height: 'auto',
+    fixedWeekCount: false,
+    buttonText: {
+      today: '今日'
+    },
+    events :[
+      {
+        start:'2022-03-18',
+        end:'2022-03-18',
+        title: '口座振替締日',
+        display: 'background'
+      }
+    ],
+    dayCellContent: function(e) {
+    e.dayNumberText = e.dayNumberText.replace('日', '');
+    },
+  });
+  calendar.render();
+});
