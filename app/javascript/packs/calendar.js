dfb40b0b112a3d5d22de9014023efcbc25ea2289
villainsff7 <villainsff790@gmail.com>
2022-03-21 07:07:29 +0000

    [Fix] calendar

--- app/javascript/packs/calendar.js
+++ app/javascript/packs/calendar.js
@@ -1,30 +1,31 @@
 import { Calendar } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import rrulePlugin from '@fullcalendar/rrule';
 
 
 document.addEventListener('turbolinks:load', function() {
   let calendarEl = document.getElementById("calendar");
   let calendar = new Calendar(calendarEl, {
-    plugins: [dayGridPlugin],
+    plugins: [dayGridPlugin,rrulePlugin],
     locale: 'ja',
     height: 'auto',
     fixedWeekCount: false,
     buttonText: {
       today: '今日'
     },
-    // events: [ , rrulePlugin,import rrulePlugin from '@fullcalendar/rrule';
-    //   {
-    //     title: '口座振替締日',
-    //     allDay: true,
-    //       rrule: {
-    //             freq: 'monthly',
-    //             count: 20,
-    //             bymonthday: [20],
-    //             dtstart: '2022-03-20',
-    //             until: '2022-12-31'
-    //         },
+    events: [
+      {
+        title: '口座振替締日',
+        allDay: true,
+          rrule: {
+                freq: 'monthly',
+                count: 20,
+                bymonthday: [20],
+                dtstart: '2022-03-20',
+                until: '2022-12-31'
+            },
 
-    // }],
+    }],
 
     dayCellContent: function(e) {
     e.dayNumberText = e.dayNumberText.replace('日', '');
