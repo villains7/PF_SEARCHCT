da731c0e8c2b89893033bb563f81e7c943968f5c
villainsff7 <villainsff790@gmail.com>
2022-03-22 10:14:34 +0000

    [Update] calendar

--- app/javascript/packs/calendar.js
+++ app/javascript/packs/calendar.js
@@ -4,45 +4,46 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 
 document.addEventListener('turbolinks:load', function() {
   let calendarEl = document.getElementById("calendar");
-  let calendar = new Calendar(calendarEl, {
-    plugins: [dayGridPlugin],
-    locale: 'ja',
-    height: 'auto',
-    fixedWeekCount: false,
-    buttonText: {
-      today: '今日'
-    },
-      events :[
-      {
-        start:'2022-03-18',
-        end:'2022-03-18',
-        title: '口座振替締日',
-        display: 'background'
+   if (calendarEl != null) {
+    let calendar = new Calendar(calendarEl, {
+      plugins: [dayGridPlugin],
+      locale: 'ja',
+      height: 'auto',
+      fixedWeekCount: false,
+      buttonText: {
+        today: '今日'
       },
-       {
-        start:'2022-04-20',
-        end:'2022-04-20',
-        title: '口座振替締日',
-        display: 'background'
-      },
-       {
-        start:'2022-05-20',
-        end:'2022-05-20',
-        title: '口座振替締日',
-        display: 'background'
-      },
-      {
-        start:'2022-06-20',
-        end:'2022-06-20',
-        title: '口座振替締日',
-        display: 'background'
-      },
-
-    ],
+        events :[
+          {
+            start:'2022-03-18',
+            end:'2022-03-18',
+            title: '口座振替締日',
+            display: 'background'
+        },
+          {
+            start:'2022-04-20',
+            end:'2022-04-20',
+            title: '口座振替締日',
+            display: 'background'
+        },
+          {
+            start:'2022-05-20',
+            end:'2022-05-20',
+            title: '口座振替締日',
+            display: 'background'
+        },
+          {
+            start:'2022-06-20',
+            end:'2022-06-20',
+            title: '口座振替締日',
+            display: 'background'
+        },
+      ],
 
     dayCellContent: function(e) {
     e.dayNumberText = e.dayNumberText.replace('日', '');
     },
   });
   calendar.render();
+   }
 });
