daffd3d8d8a85b507139cf617f6ac2150c4de8fa
villainsff7 <villainsff790@gmail.com>
2022-05-06 08:00:47 +0000

    [Add] admin_calendar

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,6 +1,29 @@
 class Admin::EventsController < ApplicationController
+  def index
+  end
   def new
     @event = events.new
     render plain: render_to_string(partial: 'form_new', layout: false,locals: {event: @event})
+  end
+
+  def create
+    @event = events.new(params_event)
+    if @event.save
+      # リクエストによって処理を分けるためrespond_toを使用
+      respond_to do |format|
+        format.html { redirect_to 'admin_events_path' }
+        # create.js.erbを探す
+        format.js
+      end
+    else
+      respond_to do |format|
+        # 登録エラーの場合はerror.js.erbを探す
+        format.js {render partial: "error"}
+      end
+    end
+  end
+
+  def params_event
+    params.require(:event).permit(:title,:start)
   end
 end
